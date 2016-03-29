3274e89fd35162da701054593168dc0fafcddddf
Heikki Kurhinen <heikki.kurhinen@gmail.com>
2016-03-29 14:42:59 +0300

    ** Added better excel export and moved fist and lastname to separate columns and added birthday column

--- public/kesatyo/js/admin.js
+++ public/kesatyo/js/admin.js
@@ -1,6 +1,11 @@
 (function(){
 	'use strict';
 	
+  $('.applicant-birthday').each(function(){
+    var birthday = moment($(this).text());
+    $(this).text(birthday.format('D.M.YYYY'));
+  })
+  
   $('.filter').change(function(){
     var queryString = '';
     $('.filter').each(function(){
@@ -10,7 +15,12 @@
   });
   
   $('#excelImport').click(function(){
-    var table = $('#applicationTable').DataTable();
+    var queryString = '';
+    $('.filter').each(function(){
+      queryString += $(this).attr('name')+'='+$(this).val()+'&';
+    });
+    window.location.href = SERVER_ROOT+'/export?'+queryString.slice(0, -1);
+    /*var table = $('#applicationTable').DataTable();
     var data = table
       .rows()
       .data();
@@ -19,7 +29,7 @@
       csvContent += i - 1 < data.length ? data[i].join(';')+ "\n" : data[i].join(',');
     }
     var encodedUri = encodeURI(csvContent);
-    window.open(encodedUri);
+    window.open(encodedUri);*/
   });
   
 	$('#menu-toggle').click(function(e) {
--- routes/components/admin.js
+++ routes/components/admin.js
@@ -1,26 +1,92 @@
 var Application = require('../../model/application');
 var _ = require('underscore');
 var config = require('../../config');
+var xlsx = require('node-xlsx');
 
-exports.renderAdminView = function(req, res){
+exports.convertPhoneNumbers = function(req, res){
+    Application.find({})
+    .batchSize(2000)
+    .exec(function(err, applications) {
+      if (err) {
+        res.status(404).send();
+      } else {
+        res.send('ok');
+        for(var i = 0; i < applications.length;i++){
+          var application = applications[i];
+          /*var fixedPhone = application.phone;
+          fixedPhone = fixedPhone.toString();
+          if(!fixedPhone.startsWith('358') && !fixedPhone.startsWith('0')){
+            fixedPhone = '0'+fixedPhone;
+          }*/
+          var phoneNumber = '';
+          for(var key in application.phone){
+            phoneNumber += key;
+            phoneNumber += application.phone[key];
+          }
+          application.phone = phoneNumber;
+          application.save(function(err, application){
+            if(err){
+              console.log(err);
+            }else{
+              console.log('Fixed phone number for application');
+            }
+          });
+        }
+      }
+    });
+};
+
+exports.renderAdminView = function(req, res) {
   var query = {};
-  if(typeof(req.query.state) !== 'undefined' && req.query.state !== ''){
+  if (typeof (req.query.state) !== 'undefined' && req.query.state !== '') {
     query.state = req.query.state;
   }
-  if(typeof(req.query.primary) !== 'undefined' && req.query.primary !== ''){
+  if (typeof (req.query.primary) !== 'undefined' && req.query.primary !== '') {
     query.primaryRequest = req.query.primary;
   }
-  if(typeof(req.query.secondary) !== 'undefined' && req.query.secondary !== ''){
+  if (typeof (req.query.secondary) !== 'undefined' && req.query.secondary !== '') {
     query.secondaryRequest = req.query.secondary;
   }
   Application.find(query)
     .sort({ added: 1 })
     .batchSize(2000)
-    .exec(function(err, applications){
-      if(err){
+    .exec(function(err, applications) {
+      if (err) {
         res.status(404).send();
-      }else{
-        res.render('admin', {user : req.user, applications: applications, state: 'Hakemukset', positions: config.positions, query: query, root: config.server_root});
+      } else {
+        res.render('admin', { user: req.user, applications: applications, state: 'Hakemukset', positions: config.positions, query: query, root: config.server_root });
+      }
+    });
+};
+
+exports.createXlsx = function(req, res) {
+  var query = {};
+  if (typeof (req.query.state) !== 'undefined' && req.query.state !== '') {
+    query.state = req.query.state;
+  }
+  if (typeof (req.query.primary) !== 'undefined' && req.query.primary !== '') {
+    query.primaryRequest = req.query.primary;
+  }
+  if (typeof (req.query.secondary) !== 'undefined' && req.query.secondary !== '') {
+    query.secondaryRequest = req.query.secondary;
+  }
+  Application.find(query)
+    .sort({ added: 1 })
+    .batchSize(2000)
+    .exec(function(err, applications) {
+      if (err) {
+        res.status(404).send();
+      } else {
+        var rows = [];
+        rows.push(['Nimi', 'Osoite', 'Puhelin', 'Email']);
+        for(var i = 0; i < applications.length;i++){
+          var application = applications[i];
+          var row = [application.firstName+' '+application.lastName, application.address+' '+application.zipcode+' '+application.city, application.phone, application.email];
+          rows.push(row);  
+        }
+        var buffer = xlsx.build([{name: 'Hakemukset', data: rows}]);
+        res.setHeader('content-type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+        res.send(buffer);
       }
     });
 };
